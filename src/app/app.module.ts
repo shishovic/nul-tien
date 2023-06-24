a4d9ece24dd2198bb4b4d71e4608f506f94c6bd3
Nemanja Šišović <nemanjashishovic@gmail.com>
2023-06-24 09:43:17 +0200

    add Toastr

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterModule } from '@angular/router';
+import { ToastrModule } from 'ngx-toastr';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
@@ -15,6 +17,8 @@ import { SharedModule } from './shared/shared.module';
     AppRoutingModule,
     RouterModule,
     SharedModule,
+    BrowserAnimationsModule,
+    ToastrModule.forRoot(),
   ],
   providers: [],
   bootstrap: [AppComponent]
